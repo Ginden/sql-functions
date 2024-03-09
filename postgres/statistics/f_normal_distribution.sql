257e83b403562dc6fbf2d2620c919ffd68c471b1
Michał Wadas <michalwadas@gmail.com>
2024-03-09 14:17:22 +0100

    [WORK IN PROGRESS]: main 2024-03-09 14:17:22+01:00

--- postgres/statistics/f_normal_distribution.sql
+++ postgres/statistics/f_normal_distribution.sql
@@ -6,7 +6,7 @@ $$
 BEGIN
     RETURN SQRT(-2 * LN(u)) * COS(2 * PI() * v);
 END;
-$$ LANGUAGE plpgsql IMMUTABLE;
+$$ LANGUAGE plpgsql IMMUTABLE STRICT;
 
 COMMENT ON FUNCTION f_normal_distribution(float8, float8) IS 'This functions converts two parameters (0 < x < 1) into a single value between 0 and 1. It is useful for generating random numbers with a normal distribution.';
 
@@ -20,7 +20,7 @@ $$
 BEGIN
     RETURN mean + stddev * f_normal_distribution(u, v);
 END;
-$$ LANGUAGE plpgsql IMMUTABLE;
+$$ LANGUAGE plpgsql IMMUTABLE STRICT;
 
 CREATE OR REPLACE FUNCTION f_random_normal(
     mean float8,
